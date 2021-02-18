a2f347c20615a1f95c5b14f759d9bf9bec860c80
Emilija Malogajski <emalogajski@gmail.com>
2021-02-18 16:20:00 +0100

    FLOM003 complete

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,27 +1,64 @@
 const dotenv = require('dotenv');
-
 dotenv.config();
 const axios = require('axios');
-
 const basePath = 'http://localhost:3000';
 
+let newThoughtRow;
+let newCell;
+let bodyOfTable;
+let newTableBody;
 
-const displayThoughts = () => {
-  const thoughts = [];
+
+const init = () => {
+  
+  const fetchThoughts = async () => {
+    newThoughtRow = document.createElement('tr');
+    try {
+      const response = await axios.get(`${basePath}/thoughts`);
+      console.log(response.data);
+      removeCurrentThoughtBody();
+      response.data.forEach(item => {
+        addNewRow(),
+        addNewCell(item);
+      })
+      // console.log(newTableBody); works!!
+    } catch (err) {
+      console.error(err);
+    }
+  };
   fetchThoughts();
-  thoughts.forEach()
 }
 
-const fetchThoughts = async () => {
-  try {
-    const response = await axios.get(`${basePath}/thoughts`);
-    // thoughts.push(response);
-    console.log(response);
-  } catch (err) {
-    console.error(err);
+window.onload = init;
+
+const removeCurrentThoughtBody = () => {
+  bodyOfTable = document.getElementById('table-body');
+  newTableBody = document.createElement('tbody');
+  bodyOfTable.parentNode.replaceChild(newTableBody, bodyOfTable);
+}
+
+const addNewRow = () => {
+  newThoughtRow.classList.add('thought-row');
+  newTableBody.append(newThoughtRow);
+  // console.log(newTableBody); works!!
+}
+
+const addNewCell = (item) => {
+  for(const property in item) {
+    newCell = document.createElement('td');
+    newCell.innerHTML = `${item[property]}`;
+    newThoughtRow.append(newCell);
   }
-};
-
-module.exports = {
-  fetchThoughts,
 }
+
+
+// const displayThoughts = () => {
+//   thoughtTextareaInput = document.getElementsByTagName('textarea').value;
+//   submitButton = document.getElementById('submitInput');
+//   submitButton.addEventListener('onclick', () => {
+//     if(thoughtTextareaInput.length !== 0) {
+//       removeCurrentThoughtRows();
+
+//     }
+//   })
+// }
